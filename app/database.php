f89ba3a4dbd27d71c672cdd9008b8267c0d15e5a
AaronMyrick <126844241+AaronMyrick@users.noreply.github.com>
2023-03-13 01:32:08 -0400

    Add files via upload

--- app/database.php
+++ app/database.php
@@ -1,9 +1,9 @@
 <?php
 
-$mysqli = new mysqli("127.0.0.1", "aaronmyrick", "UMD-3232733174","AccountInformation");
+$mysqli = new mysqli("localhost", "aaronmyrick", "UMD-3232733174","AccountInformation");
                      
 if ($mysqli->connect_errno) {
     die("Connection error: " . $mysqli->connect_error);
 }
 
-return $mysqli;
+return $mysqli
