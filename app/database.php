73fcde0a266fb3042dd2337dec584425b1903bcf
AaronMyrick <126844241+AaronMyrick@users.noreply.github.com>
2023-03-18 19:23:10 -0400

    Add files via upload

--- app/database.php
+++ app/database.php
@@ -11,10 +11,10 @@ try {
     // secure - consider a more secure solution such as
     // Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
     // keep secrets safe.
-    $username = 'root'; // e.g. 'your_db_user'
-    $password = ''; // e.g. 'your_db_password'
+    $username = 'aaronmyrick'; // e.g. 'your_db_user'
+    $password = 'UMD-123'; // e.g. 'your_db_password'
     $dbName = 'AccountInformation'; // e.g. 'your_db_name'
-    $instanceHost = '127.0.0.1'; // e.g. '127.0.0.1' ('172.17.0.1' for GAE Flex)
+    $instanceHost = '10.11.12.6'; // e.g. '127.0.0.1' ('172.17.0.1' for GAE Flex)
 
     // Connect using TCP
     $dsn = sprintf('mysql:dbname=%s;host=%s', $dbName, $instanceHost);
